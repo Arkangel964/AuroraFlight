cab202d569ee9e404121049b52d1d769209384d4
Will Whitfield <willowhitfield@outlook.com>
2023-03-08 18:22:21 -0800

    Oops i deleted all the materials hang on i need those back

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.Requests;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Networking;
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,21 +17,96 @@ public class Player : MonoBehaviour
     public int shieldTime = 0;
     public int shieldCooldown = 0;
     private GameObject shieldObj;
+    private Vector3 touchStart;   //First touch position
+    private Vector3 touchEnd;   //Last touch position
+    private float dragDistance;  //minimum distance for a swipe to be registered
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
         playerColl = GetComponent<CapsuleCollider>();
         shieldObj = transform.Find("Shield").gameObject;
+        dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                touchStart = touch.position;
+                touchEnd = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                touchEnd = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                touchEnd = touch.position;  //last touch position. Ommitted if you use list
+
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(touchEnd.x - touchStart.x) > dragDistance || Mathf.Abs(touchEnd.y - touchStart.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(touchEnd.x - touchStart.x) > Mathf.Abs(touchEnd.y - touchStart.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if ((touchEnd.x > touchStart.x))  //If the movement was to the right)
+                        {   //Right swipe
+                            lane++;
+                            if (lane > 2)
+                            {
+                                lane = 2;
+                            }
+                        }
+                        else
+                        {   //Left swipe
+                            lane--;
+                            if (lane < 0)
+                            {
+                                lane = 0;
+                            }
+                        }
+                    }
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (controller.isGrounded)
+                        {
+                            if (touchEnd.y > touchStart.y)
+                            {
+                                direction.y = jumpForce;
+
+                            }
+                        }
+                        else
+                        {
+                            direction.y += gravity * Time.deltaTime;
+                        }
+                        if(touchEnd.y < touchStart.y)
+                        {   //Down swipe
+                            if (!shield && shieldCooldown == 0 && shieldTime == 0)
+                            {
+                                shield = true;
+                                shieldObj.SetActive(true);
+                                shieldTime = 180;
+                            }
+                        }
+                    }
+                }
+                else
+                {   //It's a tap as the drag distance is less than 20% of the screen height
+                    Debug.Log("Tap");
+                }
+            }
+        }
+        
         direction.z = forwardSpeed;
         
 
-        if (controller.isGrounded)
+        /*if (controller.isGrounded)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -66,7 +141,7 @@ public class Player : MonoBehaviour
             {
                 shield = true;
                 shieldObj.SetActive(true);
-                shieldTime = 180;
+                shieldTime = 120;
             }
         }
         if (shieldTime > 0)
@@ -76,9 +151,9 @@ public class Player : MonoBehaviour
             {
                 shield = false;
                 shieldObj.SetActive(false);
-                shieldCooldown = 60;
+                shieldCooldown = 30;
             }
-        }
+        }*/
         if (shieldCooldown > 0)
         {
             shieldCooldown--;
@@ -94,7 +169,7 @@ public class Player : MonoBehaviour
             nextPosition += Vector3.right * laneOffset;
         }
         controller.Move(direction * Time.fixedDeltaTime * Time.timeScale);
-        transform.position = Vector3.Lerp(transform.position, nextPosition, laneSwitchSmoothing * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, nextPosition, laneSwitchSmoothing * Time.deltaTime * Time.timeScale);
         if(transform.position.y < -15)
         {
             Destroy(gameObject);
